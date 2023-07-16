53f2cb9a40098c55471de9f65e9b7ea1ef5d5a06
Joseph Le <13lejoseph@gmail.com>
2023-07-15 22:46:09 -0700

    Initial commit for function library

---
+++ jsfunctions.js
@@ -0,0 +1,19 @@
+// find the product of a*b
+function basicMultiply(a, b) {
+    return a * b;
+  }
+
+  //find the percentage that a is of b
+  function percentage(a, b){
+    return ((a/b)*100) + "%";
+  }
+
+  //find the cube of a number
+  function cube(a){
+    return a**3;
+  }
+
+  //find the cuberoot of a number
+  function cuberoot(a){
+    return a**(1/3)
+  }
