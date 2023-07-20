acf071a1235ac25a2d8c9b8197028aecb1533eda
Joseph Le <13lejoseph@gmail.com>
2023-07-20 12:14:42 -0700

    Add example for loop

--- jsfunctions.js
+++ jsfunctions.js
@@ -9,7 +9,7 @@ function basicMultiply(a, b) {
   }
 
 //find the cube of a number
-  function cube(a){
+  function cube(ac){
     return a**3;
   }
 
@@ -60,3 +60,107 @@ function addPercentage (number, a)
     const percentageAdded = number + ((a/100) * number)
     return percentageAdded
 }
+
+//subtract a percentage (a) to the number
+function addPercentage (number, a)
+{
+    const percentageReduced = number - ((a/100) * number)
+    return percentageReduced
+}
+
+// gets cats
+function getCats(){
+
+  const cats = ["Leopard", "Serval", "Jaguar", "Tiger", "Caracal", "Lion"];
+
+for (const cat of cats) {
+console.log(cat);
+}
+}
+
+//standard for loop
+
+for (let i = 10; i > 0; i--) {
+  
+  const para = document.createElement('p');
+  para.textContent = "hello";
+  output.appendChild(para);
+}
+
+
+const redText = document.createElement('p'); // deeclare redText create <p> element
+redText.classList.add('redText'); // add a class .redText to <p>
+redText.textContent = 'test'; // adds the text
+container.appendChild(redText); // add to .container div in HTML (<div id="container"></div>)
+
+const redText = document.createElement('p');
+redText.classList.add('redText');
+redText.textContent = 'test';
+document.body.appendChild(redText); // append to the body
+
+const redText = document.createElement('p');
+redText.classList.add('redText');
+redText.textContent = 'test';
+redText.style.color = 'red' // adds red text
+document.body.appendChild(redText);
+
+const imBlue = document.createElement('h3');
+imBlue.className = ('imBlue'); //sets class name to h3
+imBlue.textContent = "I'm a blue h3";
+imBlue.style.color = 'blue';
+document.body.appendChild(imBlue);
+
+// create a pink container, with blue border with divs inside//
+const container = document.querySelector('#container');
+const content = document.createElement('div');
+content.classList.add('contentBox');
+container.style.backgroundColor = 'pink';
+container.style.border = "thick solid #0000FF";
+content.textContent = 'test';
+
+container.appendChild(content);
+
+const imDiv = document.createElement('h1');
+imDiv.className = 'imDiv';
+imDiv.textContent = "I'm a div too";
+
+container.appendChild(imDiv);
+
+const meToo = document.createElement('p');
+meToo.className = 'meToo';
+meToo.textContent = "me too!"
+
+container.appendChild(meToo);
+
+// end script
+
+//Function Sums the min max and all numbers in between
+//create function sumALL with parameters min, max)
+const sumAll = function(min, max) {
+
+  let sum = min+max;
+//disallows negative numbers
+  if( min <0 || max <0) {return 'ERROR' }
+//if we use anything other than an integer, we get an error
+  if(!Number.isInteger(min)|| !Number.isInteger(max)){return "ERROR"}
+//if first parameter is greater than the second, will flip the order for the function to read
+  if( min > max){
+      const temp = min
+      min = max;
+      max = temp;
+  }
+  // for (define middle number; if mid is less than max; add 1 to mid)
+  for (let mid = min+1; mid < max; mid++ ){
+  // add mid to the running sum
+      sum += mid;
+      
+  }
+  return sum;
+  }
+  
+  // Do not edit below this line
+  module.exports = sumAll;
+  
+  
+  
+  
